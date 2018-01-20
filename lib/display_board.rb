88994f4fc2f509c00ac834e58027419af89ea3a0
Mars Wong <marswong618@gmail.com>
2018-01-20 06:30:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,20 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+# def display_board
+#   puts "   |   |   \n" \
+#     "-----------\n" \
+#     "   |   |   \n" \
+#     "-----------\n" \
+#     "   |   |   "
+# end
+
+# or
+def display_board
+  for i in 1..5
+    if i % 2 == 1
+      puts '   |   |   '
+    else
+      puts '-----------'
+    end
+  end
+end
